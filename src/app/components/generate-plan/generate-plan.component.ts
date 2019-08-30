6652856a705aeddb0ff5fba70ff2de8067fb04fe
abhikscube <abhikghosh@scubetc.com>
2019-08-30 13:55:14 +0530

    MultiSelect Event added
    MultiSelect Event added

--- src/app/components/generate-plan/generate-plan.component.ts
+++ src/app/components/generate-plan/generate-plan.component.ts
@@ -21,11 +21,11 @@ export class GeneratePlanComponent implements OnInit {
   constructor(public ModeljsondadaService: ModeljsondadaService) { }
 
 public brandsData:Object[]=[
-  { id: '1682', brand: 'ALL' },
-  { id: '1683', brand: 'B_Aflac_Accident' },
-  { id: '1684', brand: 'B_Aflac_Cancer' },
-  { id: '1685', brand: 'B_Aflac_Dental' },
-  { id: '1686', brand: 'B_Aflac_Hospital Ind' }
+  { id: '0', brand: 'ALL' },
+  { id: '1', brand: 'B_Aflac_Accident' },
+  { id: '3', brand: 'B_Aflac_Cancer' },
+  { id: '4', brand: 'B_Aflac_Dental' },
+  { id: '5', brand: 'B_Aflac_Hospital Ind' }
 ];
 // maps the appropriate column to fields property
 public brandsField: Object = { text: 'brand', value: 'id' };
@@ -42,21 +42,6 @@ public brandsField: Object = { text: 'brand', value: 'id' };
     {id:  26,  pid:3,name:'Sukdebpur'},
     {id:  27,  pid:3,name:'Amtala'},
     { id: 4, pid: 1, name: 'South Australia' },
-    { id: 6, pid: 1, name: 'Western Australia' },
-    { id: 7, name: 'Brazil', hasChild: true },
-    { id: 8, pid: 7, name: 'Paraná' },
-    { id: 9, pid: 7, name: 'Ceará' },
-    { id: 10, pid: 7, name: 'Acre' },
-    { id: 11, name: 'China', hasChild: true },
-    { id: 12, pid: 11, name: 'Guangzhou' },
-    { id: 13, pid: 11, name: 'Shanghai' },
-    { id: 14, pid: 11, name: 'Beijing' },
-    { id: 15, pid: 11, name: 'Shantou' },
-    { id: 16, name: 'France', hasChild: true },
-    { id: 17, pid: 16, name: 'Pays de la Loire' },
-    { id: 18, pid: 16, name: 'Aquitaine' },
-    { id: 19, pid: 16, name: 'Brittany' },
-    { id: 20, pid: 16, name: 'Lorraine' },
     { id: 21, name: 'India', hasChild: true },
     { id: 22, pid: 21, name: 'Assam' },
     { id: 23, pid: 21, name: 'Bihar' },
@@ -73,6 +58,9 @@ public showCheckBox: boolean = true;
 
     this.ModelList=this.ModeljsondadaService.modeldada;
 
+  }
+  changeValue(value){
+    console.log(value.itemData.id);
   }
 
 }
