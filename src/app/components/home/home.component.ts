23012511b7d595c0fc0b9d207d9c07923cd0317f
Abhik Ghosh <abhikghosh@scubetc.com>
2021-05-06 19:55:14 +0530

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,7 +10,7 @@ import { Subject } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-
+//test
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
   modeldataList: any;
